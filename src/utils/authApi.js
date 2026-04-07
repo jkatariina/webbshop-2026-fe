995fe760d3f5c66d68e7c76baa149385190d72a4
jkatariina <j.katariina@icloud.com>
2026-04-07 11:19:49 +0200

    add login functionality

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -1,13 +1,23 @@
 import { getBaseUrl } from "./api.js";
 
-export async function register(name, email, password) {
-    const url = new URL("auth/register", getBaseUrl());
+export async function login(email, password) {
+    const url = new URL("auth/login", getBaseUrl());
+
     const response = await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ email, password }),
     });
-    return response.json();
+
+    const data = await response.json().catch(() => ({}));
+
+    if (!response.ok) {
+        return {
+            error: data.message || "Wrong email or password"
+        };
+    }
+
+    return data;
 }
